4c7f86f6031d4473b337567afb60723e1a42e672
Simon Schippl <honeybadger26@gmail.com>
2024-05-19 21:09:27 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# kvd
+
+Get notified of new videos on given YouTube channels.
+
+## Instructions
+
+1. Create a `.env` file with your YouTube Data API key
+```
+API_KEY="<your-key-here>"
+```
+
+2. Edit `index.ts` to set the desired timeout and channels to monitor
+
+3. Install dependencies
+```bash
+bun install
+```
+
+4. Run:
+```bash
+bun run index.ts
+```
---
+++ index.ts
@@ -0,0 +1,99 @@
+type Video = {
+  channelId: string;
+  id: string;
+  title: string;
+  url: string;
+};
+
+// Time until new fetch is requested (in ms)
+const TIMEOUT = 30 * 60 * 1000;
+
+// Your YouTube API key. See here for more info:
+// https://developers.google.com/youtube/v3/getting-started
+const API_KEY = process.env.API_KEY as string;
+
+const API_URL = 'https://youtube.googleapis.com/youtube/v3';
+const VIDEO_URL = 'https://www.youtube.com/watch?v=';
+const CHANNELS = [
+  { name: 'Kendrick Lamar', id: 'UC3lBXcrKFnFAFkfVk5WuKcQ' },
+  { name: 'DrakeOfficial', id: 'UCByOQJjav0CUDwxCk-jVNRQ' },
+];
+
+function clearLine() {
+  process.stdout.write('\r\x1b[K');
+}
+
+function printLastFetchTime() {
+  const dateNow = new Date().toLocaleString('en-AU');
+  clearLine();
+  process.stdout.write(`Last fetched: ${dateNow}`);
+}
+
+async function fetchLatestVideo(channelId: string) {
+  const searchParams = new URLSearchParams({
+    key: API_KEY,
+    channelId,
+    part: 'snippet,id',
+    order: 'date',
+    maxResults: '1',
+  });
+
+  const response = await fetch(`${API_URL}/search?${searchParams}`);
+  const data = await response.json();
+  const video = data.items[0];
+
+  return {
+    channelId,
+    id: video.id.videoId,
+    title: video.snippet.title,
+    url: VIDEO_URL + video.id.videoId,
+  };
+}
+
+let savedLatestVideos: null | Video[] = null;
+
+async function fetchVideos() {
+  if (!savedLatestVideos) {
+    console.log('Latest videos:');
+    savedLatestVideos = [];
+
+    for (const channel of CHANNELS) {
+      process.stdout.write(`\tFetching latest from ${channel.name}...`);
+      const latestVideo = await fetchLatestVideo(channel.id);
+      savedLatestVideos.push(latestVideo);
+      clearLine();
+      console.log(`\t${channel.name}: [${latestVideo.title}](${latestVideo.url})`);
+    }
+
+    printLastFetchTime();
+    return;
+  }
+
+  clearLine();
+  process.stdout.write('Fetching...');
+
+  for (const channel of CHANNELS) {
+    const latestVideo = await fetchLatestVideo(channel.id);
+    const savedLatestVideo = savedLatestVideos.find(
+      (video) => video.channelId === channel.id
+    )!;
+
+    if (latestVideo.id !== savedLatestVideo.id) {
+      const message = `>>> NEW VIDEO ON ${channel.name}: [${latestVideo.title}](${latestVideo.url}) <<<`;
+      clearLine();
+      console.log('>'.repeat(message.length));
+      console.log(message);
+      console.log('<'.repeat(message.length));
+    }
+  }
+
+  printLastFetchTime();
+}
+
+fetchVideos();
+const interval = setInterval(fetchVideos, TIMEOUT);
+
+process.on('SIGINT', function() {
+  console.log('\nExiting...');
+  clearInterval(interval);
+});
